7c6ff4390b91765e748727d3be22e158f08753d3
Ruan <ruanmateus344@gmail.com>
2024-09-19 06:36:07 -0300

    Array de produtos criado e testado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// seção de Script para o Modal
+// Evento para abrir e fechar o modal (popup)
 let modal = document.querySelector('#modal-container')
 
 document.querySelector('#add-product').addEventListener('click', function() {
@@ -9,5 +9,22 @@ document.querySelector('#add-product').addEventListener('click', function() {
             modal.classList.remove('abrir')
         }
     })
+})
+
+// Evento
+let products = [
+    { id: 1, nome: 'Camiseta Azul', categoria: 'Camisetas', preco: 80 }, 
+    { id: 1, nome: 'Air Jordan 1 High OG', categoria: 'Calçados', preco: 750 },
+]
+
+document.querySelector('#btn-cadastrar').addEventListener('click', function() {
+    let name = document.querySelector('#iproduct').value
+    let category = document.querySelector('#icategory').value
+    let price = document.querySelector('#iprice').value
+
+    let id = products.length ? products[products.length - 1].id + 1 : 1;
+    let newProduct = { id, name, category, price };
+    products.push(newProduct);
+    console.log(products)
 })
 
