4f8c3e6dddaf74c8fc6ee9bf2d9bb6640d8afefa
Ruan <ruanmateus344@gmail.com>
2024-09-20 06:08:58 -0300

    Update
    Atualização do script do botão de cadastrar produtos, que antes era selecionado pela DIV. Criei uma forma do usuário selecionar e adicionar a imagem do produto que ele deseja publicar (a imagem deve ter no máximo 450px). Também foi criado um sistema de alertas para guiar o usuário na criação do produto, para caso esteja faltando informações ou se a imagem enviada passar do limite de pixels. Lista de produtos publicados implementada e rodapé atualizado.

--- script.js
+++ script.js
@@ -11,20 +11,72 @@ document.querySelector('#add-product').addEventListener('click', function() {
     })
 })
 
-// Evento
+// Array criado para armazenar os produtos adicionados pelo usuário
 let products = [
-    { id: 1, nome: 'Camiseta Azul', categoria: 'Camisetas', preco: 80 }, 
-    { id: 1, nome: 'Air Jordan 1 High OG', categoria: 'Calçados', preco: 750 },
+    { id: 1, name: 'Camiseta Azul', category: 'Camisetas', price: 80, imageUrl: '' }, 
+    { id: 2, name: 'Air Jordan 1 High OG', category: 'Calçados', price: 750, imageUrl: '' },
 ]
 
-document.querySelector('#btn-cadastrar').addEventListener('click', function() {
-    let name = document.querySelector('#iproduct').value
-    let category = document.querySelector('#icategory').value
-    let price = document.querySelector('#iprice').value
+// Função para publicar o produto descrito pelo usuário
+function publishProduct(product) {
+    addProductList = document.querySelector('#products-list');
+    addProductList.innerHTML += `
+    <li class="products">
+        <img class="preview" src="${product.imageUrl}" alt="Imagem do Produto" width="100"/>
+        <p><span class="product-category"><span class="material-symbols-outlined category-icon">category</span>${product.category}</span> <span class="product-name">${product.name}</span> <span class="product-price">R$ ${product.price}</span></p>
+    </li>
+`;
+}
 
-    let id = products.length ? products[products.length - 1].id + 1 : 1;
-    let newProduct = { id, name, category, price };
-    products.push(newProduct);
-    console.log(products)
-})
+// Função que cria o preview da imagem do produto escolhido pelo usuário
+function readImage(inputElement, callback) {
+    if (inputElement.files && inputElement.files[0]) {
+        let file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("preview").src = e.target.result;
+            if (callback) callback(e.target.result); // Passa a URL da imagem de volta via callback
+        };
+        file.readAsDataURL(inputElement.files[0]);
+    }
+}
+
+// Evento para cadastro de novos produtos
+document.querySelector('#btn-cadastrar').addEventListener('click', function() {
+    let name = document.querySelector('#iproduct').value;
+    let category = document.querySelector('#icategory').value;
+    let price = parseFloat(document.querySelector('#iprice').value);
+    let alerts = document.querySelector('#alerts');
+
+    if (!name || !category || isNaN(price)) {
+        alerts.innerHTML = 'Por favor, preencha todos os campos do produto.'
+        return;
+    }
+
+    let uploadImg = document.getElementById("upload-img");
+
+    readImage(uploadImg, function(imageUrl) {
+        let img = new Image();
+        img.src = imageUrl;
+
+        img.onload = function() {
+            // Condicional criada para verificar se a largura natural da imagem é maior que 450px
+            if (img.naturalWidth > 450) {
+                alerts.innerHTML = 'A imagem enviada pode ter no máximo 450px de largura.';
+                return;
+            }
+
+            let id = products.length ? products[products.length - 1].id + 1 : 1;
+            let newProduct = { id, name, category, price, imageUrl };
+            products.push(newProduct);
+
+            publishProduct(newProduct);
+        };
+    });
+});
+
+// Função para remover produtos
+function productRemove(id) {
+    products = products.filter(produto => produto.id !== id);
+
+}
 
